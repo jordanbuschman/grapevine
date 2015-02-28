619706a98487a9c70ab92a946236be167abd8cee
Jordan Buschman <jordan.buschman@me.com>
2015-02-28 15:54:07 -0500

    debug stuff and notes from gus

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+//see any posts
+//see comments
+//create posts
+//fetch location
+
 module.exports = router;
