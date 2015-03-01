01fe2d30a48c84e94f93eb2eb7b16184d15172f4
Jordan Buschman <jordan.buschman@me.com>
2015-02-28 18:20:50 -0800

    meow

--- models/post.js
+++ models/post.js
@@ -4,7 +4,8 @@ var Schema                = mongoose.Schema;
 
 var Post = new Schema({
 	_id			: {type: String, required: true },
-	_parent		: {type: Schema.Types.ObjectId, ref: 'Post', required: true},
+	_parent		: {type: Schema.Types.ObjectId, ref: 'Post', required: true, default: -1},
+	_root		: {type: Schema.Types.ObjectId, ref: 'Post', required: true, default: -1},
 	_timestamp	: {type: Date, default: new Date() },
 	_text		: {type: String, required: true},
 	_views		: {type: Number, default: 0}
