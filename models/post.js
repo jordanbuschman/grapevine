a54ee48ad4b55fb994f222c56c982f3642bd0dec
Jordan Buschman <jordan.buschman@me.com>
2015-02-28 23:54:49 -0800

    Test shit

--- models/post.js
+++ models/post.js
@@ -3,15 +3,15 @@ var Schema                = mongoose.Schema;
 
 
 var Post = new Schema({
-	_parent		: {type: Schema.Types.ObjectId, ref: 'Post' },
-	_root		: {type: Schema.Types.ObjectId, ref: 'Post' },
-	_location	: {type: String, required: true },
-	_grape		: {type: Number, required: true },
-	_timestamp	: {type: Date, default: new Date() },
-	_text		: {type: String, required: true},
-	_views		: {type: Number, default: 0},
-	_userID		: {type: String, required: true },
-	_username	: {type: String },
+	_parent		 : {type: Schema.Types.ObjectId, ref: 'Post' },
+	_root		 : {type: Schema.Types.ObjectId, ref: 'Post' },
+	_location	 : {type: String, required: true },
+	_grape		 : {type: Number, required: true },
+	_timestamp	 : {type: Date, default: new Date() },
+	_text		 : {type: String, required: true},
+	_views	 	 : {type: Number, default: 0},
+    _phoneNumber : {type: Number, required: true{,
+	_username	 : {type: String },
 });
 
 
