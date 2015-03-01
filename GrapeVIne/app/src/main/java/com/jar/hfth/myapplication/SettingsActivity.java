9ef0858e18094901ea4ec282cfdd925d3ba2d96c
AugustBeyer <xylaphonies@gmail.com>
2015-02-28 17:25:59 -0800

    fixed POST method
    communicates with server

--- GrapeVIne/app/src/main/java/com/jar/hfth/myapplication/SettingsActivity.java
+++ GrapeVIne/app/src/main/java/com/jar/hfth/myapplication/SettingsActivity.java
@@ -5,13 +5,27 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -21,6 +35,15 @@ import java.util.Locale;
 public class SettingsActivity extends ActionBarActivity {
 
     GPSTracker gps;
+    DefaultHttpClient client = new DefaultHttpClient();
+    HttpPost post = new HttpPost("http://getgrapes.org");
+
+    public void enableStrictMode()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,20 +89,44 @@ public class SettingsActivity extends ActionBarActivity {
                     double longitude = gps.getLongitude();
                     Geocoder geocoder = new Geocoder(SettingsActivity.this, Locale.getDefault());
                    try{
+                       //get city/state from lat and long
                        List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
                        String cityName = addresses.get(0).getAddressLine(0);
                        String stateName = addresses.get(0).getAddressLine(1);
                        String countryName = addresses.get(0).getAddressLine(2);
+
+                      //set the text view to update
                        TextView textView = (TextView) findViewById(R.id.textL);
-                       textView.setText(cityName + ", " + stateName);
+                       textView.setText(stateName);
+
+                       //toast notification
+                       Toast.makeText(getApplicationContext(), "Your Location is "+ stateName , Toast.LENGTH_SHORT).show();
+
+                      //send the data to back end
+                       /*
+                       List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+                       pairs.add(new BasicNameValuePair("location", stateName));
+                       post.setEntity(new UrlEncodedFormEntity(pairs));
+                       HttpResponse response = client.execute(post);   */
+
+                       try {
+                           enableStrictMode();
+                           JSONObject result = new JSONObject();
+                           result.put("location", stateName);
+                           StringEntity se = new StringEntity(result.toString());
+                           post.setEntity(se);
+                           HttpResponse httpresponse = client.execute(post);
+                       } catch (Exception e) {
+                           //throw new RuntimeException(e);
+                           Toast.makeText(getApplicationContext(), "cannot communicate with server" , Toast.LENGTH_SHORT).show();
+                           Log.e("MYAPP", "exception", e);
+                       }
+
                    }catch(IOException ex) {
                        ex.printStackTrace();
                    }
 
 
-
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
                 }else{
                     // can't get location
                     // GPS or Network is not enabled
